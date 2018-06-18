f955773162d7f2c1e2117ca081fdac7ffe6cb631
SilencerWeb <silencer.web@gmail.com>
2018-06-18 19:21:57 +0500

    Rewrite RetinaImage with prop-types instead of flow

--- src/ui/atoms/retina-image.js
+++ src/ui/atoms/retina-image.js
@@ -1,16 +1,6 @@
-// @flow
 import * as React from 'react';
 import styled, { css } from 'styled-components';
-
-
-type props = {
-  className?: string,
-  src: {
-    _1x: string,
-    _2x: string,
-  },
-  alt: string,
-};
+import PropTypes from 'prop-types';
 
 
 const Wrapper = styled.div`
@@ -18,7 +8,7 @@ const Wrapper = styled.div`
 `;
 
 
-export const RetinaImage = (props: props) => {
+export const RetinaImage = (props) => {
   return (
     <img
       className={ props.className }
@@ -27,4 +17,13 @@ export const RetinaImage = (props: props) => {
       alt={ props.alt }
     />
   );
+};
+
+RetinaImage.propTypes = {
+  className: PropTypes.string,
+  src: PropTypes.shape({
+    _1x: PropTypes.string.isRequired,
+    _2x: PropTypes.string.isRequired,
+  }).isRequired,
+  alt: PropTypes.string.isRequired,
 };
